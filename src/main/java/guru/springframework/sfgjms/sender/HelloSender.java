9deec0219771d3748c4b37ee0689e345164fd614
Dorothy Rifai <drifai28@gmail.com>
2020-07-20 08:16:59 -0400

    JMS example which sends response when receiving a message

--- src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
+++ src/main/java/guru/springframework/sfgjms/sender/HelloSender.java
@@ -2,9 +2,17 @@ package guru.springframework.sfgjms.sender;
 
 import java.util.UUID;
 
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.Session;
+
 import org.springframework.jms.core.JmsTemplate;
+import org.springframework.jms.core.MessageCreator;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import guru.springframework.sfgjms.config.JmsConfig;
 import guru.springframework.sfgjms.model.HelloWorldMessage;
@@ -15,6 +23,7 @@ import lombok.RequiredArgsConstructor;
 public class HelloSender {
 	
 	private final JmsTemplate jmsTemplate;
+	private final ObjectMapper objectMapper;
 
 	@Scheduled(fixedRate = 2000)
 	public void sendMessage() {
@@ -30,5 +39,35 @@ public class HelloSender {
 		jmsTemplate.convertAndSend(JmsConfig.MY_QUEUE, message);
 		
 		System.out.println("Message sent!");
+	}
+	
+	@Scheduled(fixedRate = 2000)
+	public void sendAndReceiveMessage() throws JMSException {
+		System.out.println("Sending a message...");
+
+		HelloWorldMessage message = HelloWorldMessage.builder().id(UUID.randomUUID()).message("Hello").build();
+
+		// use overloaded version with destination and message
+		Message receivedMessage = jmsTemplate.sendAndReceive(JmsConfig.MY_SEND_RCV_QUEUE, new MessageCreator() {
+
+			@Override
+			public Message createMessage(Session session) throws JMSException {
+				Message helloMessage;
+				try {
+					helloMessage = session.createTextMessage(objectMapper.writeValueAsString(message));
+					helloMessage.setStringProperty("_type", "guru.springframework.sfgjms.model.HelloWorldMessage");
+				} catch (JsonProcessingException e) {
+					throw new JMSException("ka-boom");
+				} catch (JMSException e) {
+					throw e;
+				}
+				
+				System.out.println("Sending Hello");
+				return helloMessage;
+			}
+
+		});
+
+		System.out.println(receivedMessage.getBody(String.class));
 	}
 }
